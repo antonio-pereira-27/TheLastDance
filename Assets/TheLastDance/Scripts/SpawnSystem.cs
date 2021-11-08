b2febb9fb07b972127375ba4f3fc294e3781d444
careca-gif <73558853+careca-gif@users.noreply.github.com>
2021-11-08 16:52:18 +0000

    Falta ajustes

--- Assets/TheLastDance/Scripts/SpawnSystem.cs
+++ Assets/TheLastDance/Scripts/SpawnSystem.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnSystem : MonoBehaviour
 {
+    // variáveis
     public GameObject enemyPrefab;
 
     public PlayerMovement player;
@@ -20,28 +19,36 @@ public class SpawnSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // diminuir o timer
         timer -= Time.deltaTime;
 
         if (timer < 0)
         {
-            timer = 20f;
+            // reiniciar o contador de tempo
+            timer = 10f;
             
+            // verifica se os todos os inimigos ja foram instanciados
             if (enemiesSpawned < totalEnemies)
             {
+                // definir o ponto para instanciar os inimigos
                 Transform spawnPoint = spawnPoints[Random.Range(0,spawnPoints.Length)];
-                GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
-                Target enemyAi = enemy.GetComponent<Target>();
+                GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity); // instanciar o inimigo
+                Target enemyAi = enemy.GetComponent<Target>(); // pegar na componente target
+                // atribuir valores as variáveis da componente
                 enemyAi.enemyTransform = player.transform;
                 enemyAi.spawnSystem = this;
+                // contar os inimigos que ja foram instanciados
                 enemiesSpawned++;
             }
         }
     }
 
+    // função para quando contar inimigos mortos e quando todos assim estiverem este termina o jogo
     public void EnemyEliminated()
     {
         enemiesEliminated++;
 
+        // verifica se o total de inimigos é o mesmo que os que já morreram
         if (totalEnemies == enemiesEliminated)
         {
             Time.timeScale = 0;
--- Assets/TheLastDance/Scripts/Target.cs
+++ Assets/TheLastDance/Scripts/Target.cs
@@ -7,51 +7,57 @@ using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour
 {
-   // variáveis
+   // velociade
    public float speed = 7f;
-   public float attackDistance = 7f;
+   // vida
    public float health = 100f;
-   private float _nextAttackTime;
-   public float attackRate = 5f;
    public float currentHealth;
+   // ataque
    private float _damage = 10f;
-   private float timer = 50f;
+   public float attackDistance = 7f;
+   private float timerToAttack = 0f;
+   // movimentação
    private bool chasing = false;
-   private float attackDuration = 0f;
+   private float timer = 50f;
    
-
+   // barra de vida
    public HealthBar healthBar;
-
-   [HideInInspector]
-   public Transform enemyTransform;
-
+   // transform do player
+   [HideInInspector] public Transform enemyTransform;
+   // sistema de spawn de inimigos
    [HideInInspector] public SpawnSystem spawnSystem;
 
+   // navmeshagent para npc
    private NavMeshAgent _agent;
-
+   // rigidbody
    private Rigidbody _rigidbody;
-   
+   // sistema de particulas para efeito de disparo
    public ParticleSystem muzzleFlash;
 
    // função de inicio 
    private void Start()
    {
+      // inicializar a navmeshagent no script e atribuir valores
       _agent = GetComponent<NavMeshAgent>();
-      //_agent.stoppingDistance = attackDistance;
+      _agent.stoppingDistance = attackDistance;
       _agent.speed = speed;
+      // inicializar o rigidbody através de script
       _rigidbody = GetComponent<Rigidbody>();
       _rigidbody.useGravity = false;
       
+      // atualizar a barra de vida do npc
       currentHealth = health;
       healthBar.SetMaxHealth(health);
-
       
    }
 
+   // função update que atualiza a cada frame
    void Update()
    {
-      if (!chasing)
+      // condição para verificar se o jogador está no campo de visão do npc ou se este o atacou pelas costas
+      if (!chasing || currentHealth >= health)
       {
+         // contador de tempo para mudanças de direção
          if (timer > 5f)
          {
             RandomDirection(10f);
@@ -59,93 +65,95 @@ public class Target : MonoBehaviour
          
          }
          else
-         {
-            timer += Time.deltaTime;
-         }
+            timer += Time.deltaTime; // aumenta o contador
          
-
+         
+         // procura pelo jogador através do sistema de raycast do unity
          RaycastHit hitSearching;
-         Debug.DrawRay(transform.position, transform.forward * 7, Color.magenta, 1f);
+         //Debug.DrawRay(transform.position, transform.forward * 7, Color.magenta, 1f);
          if (Physics.Raycast(transform.position, transform.forward, out hitSearching, attackDistance))
          {
+            // verifica se é o jogador através da tag
             if (hitSearching.transform.CompareTag("Player"))
             {
                Debug.Log("I found the player");
-               chasing = true;
+               chasing = true; // atualizar a variável de perseguição
             }
          }
          
       }
       else
       {
-         
+         // olha para o jogador e persegue-o
          transform.LookAt(enemyTransform.transform.position);
          _agent.destination = enemyTransform.position;
-         
-         if (Time.time > _nextAttackTime)
-         {
-            attackDuration = 0.5f;
-            _nextAttackTime = Time.time + attackRate;
-         }
-         
-         if(attackDuration > 0f)
-         {
-            attackDuration -= Time.deltaTime;
-            RaycastHit hitAttack;
 
-            Debug.DrawRay(transform.position, transform.forward * 7, Color.green, 1f);
+         // contador para atacar o jogador
+         if (timerToAttack > 1.5f)
+         {
+            // reiniciar o contador
+            timerToAttack = 0f;
+            
+            //novamente sistema de raycast do unity para acertar no jogador
+            RaycastHit hitAttack;
+            //Debug.DrawRay(transform.position, transform.forward * 7, Color.green, 1f);
             if (Physics.Raycast(transform.position, transform.forward, out hitAttack, attackDistance))
             {
+               // verifica se é o jogador através da tag
                if (hitAttack.transform.CompareTag("Player"))
                {
                   Debug.Log("Ill shoot");
-                  
+                  // verifica a componente de jogar e tira lhe vida
                   PlayerMovement player = hitAttack.transform.GetComponent<PlayerMovement>();
                   player.TakeDamage(_damage);
-               
+                  // inicia o sistema de particulas
                   muzzleFlash.Play();
                }
             }
-
-
          }
+         else
+            timerToAttack += Time.deltaTime; // aumenta o contador
          
       }
-      
-
    }
    
-
+   // função para perder vida quando o jogador dispara sobre este
    public void TakeDamage(float amount)
    {
       currentHealth -= amount;
       healthBar.SetHealth(currentHealth);
-      if (currentHealth <= 0f)
+      if (currentHealth <= 0f) // verifica se morreu
       {
          Die();
       }
    }
 
+   // função para destruir o gameObject
    void Die()
    {
       Destroy(gameObject);
       spawnSystem.EnemyEliminated();
    }
 
+   // função para dar uma direção aleatória ao npc
    public void RandomDirection(float radius)
    {
+      // através de um angulo definido entre -pi e pi
       float angle = Random.Range(-Mathf.PI, Mathf.PI);
+      // a direção que o inimigo vai seguir é feita através do seno do angulo para o eixo X e através do cosseno para o eixo Z
       Vector3 randomDirection = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * radius;
+      // a esta direção adiciona-se a posição do npc
       randomDirection += transform.position;
+      
       NavMeshHit navMeshHit;
       Vector3 finalPosition = Vector3.zero;
-      
-
+      // através da navmeshhit do unity é feito o ponto final para onde o npc irá seguir
       if (NavMesh.SamplePosition(randomDirection, out navMeshHit, radius, 1))
       {
+         // a sua posição final
          finalPosition = navMeshHit.position;
          //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = finalPosition;
-         _agent.SetDestination(finalPosition);
+         _agent.SetDestination(finalPosition); // destino do npc
       }
       
    }
